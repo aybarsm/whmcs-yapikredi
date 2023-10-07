30d6c48cf04abee8a06f7be9e1bc35a73f154454
Murat Aybars <hello@aybarsm.com>
2023-10-07 10:35:49 +0100

    Config file structure change

--- config/gw.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Aybarsm\Whmcs\Gateway\Yapikredi\Posnet;
-use Aybarsm\Whmcs\Service\Whmcs as WhmcsService;
-
-return (Posnet::isModuleActive() ? WhmcsService::getGatewaySettings(Posnet::getIdentifier()) : []);
--- config/metadata.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'DisplayName' => 'Yapi Kredi Sanal Pos',
-    'gatewayType' => 'CC',
-    'APIVersion' => '1.1',
-];
